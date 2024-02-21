bc77dd6c5cd4bc2ab16195349d8975786713ae1a
Zaid Kamil <xaidmetamorphos@gmail.com>
2024-02-21 14:08:50 +0530

    strings

---
+++ pattern2.py
@@ -0,0 +1,15 @@
+from turtle import *
+speed(0)
+
+def polygon(side, size):
+    for i in range(side):
+        fd(size)
+        lt(360/side)
+
+# calling
+for i in range(6):
+    fd(100)
+    polygon(6, 80)
+    lt(60)
+
+mainloop()
